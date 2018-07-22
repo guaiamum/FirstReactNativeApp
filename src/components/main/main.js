afd8424326a87b376cff239381a36cc8b9e2e437
Cainã Brazil <cainabrazil@gmail.com>
2018-07-22 21:03:42 +0100

    restyling almost done

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,57 +1,76 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
   StyleSheet, View, Image, Text, TextInput, KeyboardAvoidingView, StatusBar
 } from 'react-native';
 
 
-
 export default class Main extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      result: 20,
+      kmPerLiter: '20',
+      literPerHundred: '5',
       warning: ""
     }
     resultado = (result) => {
       var _result = 20, _warning = "";
       if (result != "" || result > 0) {
-        _result = (100 / result).toFixed(2);
-        if (isNaN(_result) || _result < 0){
+        _result = this.EuToBr(result);
+        if (isNaN(_result) || _result < 0) {
           _result = "?";
-          if(result.contains(',')){_warning = "try using a \".\" instead of a comma!"}
+          if (result.contains(',')) { _warning = "try using a \".\" instead of a comma!" }
         }
       }
-      this.setState({result: _result,warning: _warning});
+      this.setState({ kmPerLiter: _result, warning: _warning });
     }
   }
+
+  BrToEu = (kmPerLiter) => {
+    return (kmPerLiter / 100).toFixed(2);
+  }
+
+  EuToBr = (literPerHundred) => {
+    return (100 / literPerHundred).toFixed(2);
+  }
+
   render() {
+    const state = this.state;
     return (
       <KeyboardAvoidingView behavior="padding" style={styles.container}>
-      <StatusBar
-        barStyle="light-content"
-      />
-      <View style={styles.logoContainer}>
-        <Image
-          style={styles.logo}
-          source={require('../../images/main.png')}
+        <StatusBar
+          barStyle="light-content"
         />
+        <View style={styles.logoContainer}>
+          <Image
+            style={styles.logo}
+            source={require('../../images/main.png')}
+          />
+        </View>
+        <View style={styles.measureContainer}>
+          <TextInput
+            underlineColorAndroid='rgba(0,0,0,0)'
+            keyboardType="numeric"
+            placeholder="20 km per liter"
+            style={styles.input}
+            value={state.kmPerLiter}
+            onChangeText={(text) => { resultado(text) }}
+          />
+          <Text style={styles.measureLabel} >km/l</Text>
+        </View>
 
-        <Text style={styles.result} >
-          {this.state.result} km/l
-        </Text>
-        <Text style={styles.small} >
-          {this.state.warning}
-        </Text>
-      </View>
+        <Text style={styles.small} >{state.warning}</Text>
 
-      <TextInput
-        underlineColorAndroid= 'rgba(0,0,0,0)'
-        keyboardType="numeric"
-        placeholder="5 liters per 100 km"
-        style={styles.input}
-        onChangeText={(text) => {resultado(text)}}
-      />
+        <View style={styles.measureContainer}>
+          <Text style={styles.measureLabel} >liters per 100 km</Text>
+          <TextInput
+            underlineColorAndroid='rgba(0,0,0,0)'
+            keyboardType="numeric"
+            style={styles.input}
+            value={state.literPerHundred}
+            onChangeText={(text) => { resultado(text) }}
+          />
 
+        </View>
       </KeyboardAvoidingView>
     );
   }
@@ -62,31 +81,36 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#61082b'
   },
-  small: {
-    color: 'rgba(255,255,255,0.3)',
-    fontSize: 9,
-  },
-  logo: {
-    width: 80,
-    height: 80,
-    marginTop: 100,
-    marginBottom: 4
-  },
   logoContainer: {
     alignItems: 'center',
     justifyContent: 'center',
   },
-  result: {
-    color: 'white',
-    fontSize: 35,
-    marginTop: 8
+  logo: {
+    width: 40,
+    height: 40,
+    marginTop: 4,
+    marginBottom: 4
+  },
+  measureContainer: {
+    //alignItems: 'center',
+    justifyContent: 'center',
+  },
+  small: {
+    color: 'rgba(255,255,255,0.3)',
+    fontSize: 9,
+  },
+  measureLabel: {
+    fontFamily: 'JosefinSans-Thin',
+    color: '#ccc',
+    fontSize: 15,
+    alignSelf: 'center'
   },
   input: {
-    height: 40,
+    fontSize: 150,
+    height: 250,
     margin: 10,
-    paddingHorizontal: 5,
     textAlign: 'center',
-    backgroundColor: 'rgba(255,255,255,0.4)',
+    //backgroundColor: 'rgba(255,255,255,0.4)',
     color: 'white'
   }
 });
